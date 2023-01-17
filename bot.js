c52e4a6b6710d12461b9bdab9e5ea9f1309de7e7
Justin <32044262+justinmok@users.noreply.github.com>
2023-01-16 22:55:01 -0800

    aboard javascript hate train

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const davinciConfig = {
         temperature: 1,
         max_tokens: 300,
         top_p: 1,
-        echo: true,
+        echo: false,
         frequency_penalty: 0,
         presence_penalty: 0,
 }
@@ -33,13 +33,20 @@ client.on('interactionCreate', async interaction => {
 
     const allMessages = await interaction.channel.messages.fetch();
     const lastHour = allMessages.filter(msg => msg.createdTimestamp > Date.now() - (3600000 * 10));
-    const transcript = lastHour.map((map, index, arr) => {
-        let lastAuthor;
-        if (index != 0) lastAuthor = arr[index - 1].author;
-        if (lastAuthor && lastAuthor == msg.author.username) return msg.content;
-        else return `${msg.author.username} - ${msg.content}`
-    });
+    const transcript = (Array.from(lastHour.values())).map((msg, index, arr) => {
+        if (msg.content.trim().length == 0) return;
 
+        let lastAuthor;
+        if (index + 1 < arr.length) lastAuthor = (Array.from(arr))[index + 1].author
+
+        if (lastAuthor && lastAuthor.username == msg.author.username) {
+            if (msg.content.trim().length > 0) return msg.content;
+        }
+
+        else return `${msg.author.username} - ${msg.content}`
+    }).filter(chat => chat);
+
+    console.log(transcript.reverse().join('\n'));
     if (lastHour.size == 0) return interaction.reply('There were no messages sent within the last hour.')
     
     interaction.deferReply()
@@ -51,8 +58,9 @@ client.on('interactionCreate', async interaction => {
     
     const summary = response.data.choices[0].text;
     console.log(summary);
-    if (summary) interaction.editReply(`\`\`\`${JSON.stringify(davinciConfig)}\nPrompt: Your task is to label the different conversation topics in the following set of text formatted as a chat log. You must pick out each seperate topic and display them in bullet point format. Do not add any excess text outside the bullet points.\`\`\`\nDuring the past hour, there were ${lastHour.size} messages sent. Here is a summary of those messages: \n${summary}`);
-    else interaction.editReply('random error LMAO')
+    //if (summary) interaction.editReply(`\`\`\`${JSON.stringify(davinciConfig)}\nPrompt: Your task is to label the different conversation topics in the following set of text formatted as a chat log. You must pick out each seperate topic and display them in bullet point format. Do not add any excess text outside the bullet points.\`\`\`\nDuring the past hour, there were ${lastHour.size} messages sent. Here is a summary of those messages: \n${summary}`);
+    //else
+    //interaction.editReply('random error LMAO')
     
 } catch (e) {
     console.log(e);
